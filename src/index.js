d0c090fed6cbe30caed829262b7bfa83e3f3ff07
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-08-25 00:11:55 -0400

    rename App to GraphIDE to avoid confusion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import GraphIDE from './GraphIDE';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GraphIDE />, document.getElementById('root'));
